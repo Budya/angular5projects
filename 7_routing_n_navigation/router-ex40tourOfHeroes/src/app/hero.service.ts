11f6f7ea0baaa3ba8fe5f4195257fcd744a8d070
Dima <budya2012@gmail.com>
2020-11-04 21:25:47 +0300

    tour of heroes done_before router sample 041120n

--- 7_routing_n_navigation/router-ex40tourOfHeroes/src/app/hero.service.ts
+++ 7_routing_n_navigation/router-ex40tourOfHeroes/src/app/hero.service.ts
@@ -13,6 +13,12 @@ export class HeroService {
   
   private heroesUrl = 'api/heroes';
 
+  httpOptions = {
+    headers: new HttpHeaders(
+      {'Contend-Type': 'application/json'}
+    )
+  };
+
   constructor(private messageService: MessageService,
               private http: HttpClient) { }
 
@@ -57,5 +63,45 @@ export class HeroService {
 
       return of(result as T);
     }
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    )
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    )
+  }
+
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions)
+               .pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))          
+    );
+  }
+
+  searchHeroes(term: string): Observable<Hero[]>{
+    if(!term.trim()){
+      return of([]);
+    }
+
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`)
+    .pipe(
+      tap(x => x.length ? 
+        this.log(`found heroes matching "${term}"`) : 
+        this.log(`no heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))  
+    
+    );
   }
 }
