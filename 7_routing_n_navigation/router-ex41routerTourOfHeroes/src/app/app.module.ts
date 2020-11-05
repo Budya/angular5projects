2717bd9c4c2567c36a3ec6c7bdf9d0a2dd7370f7
Dima <budya2012@gmail.com>
2020-11-05 20:33:25 +0300

    milestone 2

--- 7_routing_n_navigation/router-ex41routerTourOfHeroes/src/app/app.module.ts
+++ 7_routing_n_navigation/router-ex41routerTourOfHeroes/src/app/app.module.ts
@@ -1,34 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
-import { RouterModule, Routes } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const appRoutes: Routes = [
-  { path: 'crisis-center', component: CrisisListComponent},
-  { path: 'heroes', component: HeroListComponent},
-  { path: '', redirectTo: '/heroes', pathMatch: "full"},
-  { path: '**', component: PageNotFoundComponent}
-];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CrisisListComponent,
-    HeroListComponent
+    HeroListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(
-      appRoutes,
-      {enableTracing: true}
-    )
+    AppRoutingModule    
     ],
   providers: [],
   bootstrap: [AppComponent]
