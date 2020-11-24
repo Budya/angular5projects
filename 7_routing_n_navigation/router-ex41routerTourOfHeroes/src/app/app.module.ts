bbe168198f19365556f403c3560e82c5f0669617
Dima <budya2012@gmail.com>
2020-11-24 14:51:36 +0300

    canActivateChild done 201124

--- 7_routing_n_navigation/router-ex41routerTourOfHeroes/src/app/app.module.ts
+++ 7_routing_n_navigation/router-ex41routerTourOfHeroes/src/app/app.module.ts
@@ -23,7 +23,7 @@ import { AuthModule } from './auth/auth.module';
     //HeroListComponent,
     PageNotFoundComponent,
     ComposeMessageComponent,
-    LoginComponent
+    
     
   ],
   imports: [
