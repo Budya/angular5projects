8b9d5c2a45edc4811c05f15bd2192fd1213faa3e
Dima <budya2012@gmail.com>
2020-11-27 16:55:48 +0300

    http-ex200 stage 201127

---
+++ 10_HTTP/http-ex100httpGet/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { SwaggerService } from './swagger.service';
+import { Language } from './language';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>Countries</h1>
+    <ul>
+      <li *ngFor="let language of _languages">
+        {{language.name}}&nbsp;({{language.code}});
+      </li>
+    </ul>
+    
+  `,
+  styles: []
+})
+export class AppComponent implements OnInit {
+  title = 'http-ex100httpGet';
+  
+  _languages = new Array<Language>();
+
+  constructor(private _swaggerService: SwaggerService){}
+
+  ngOnInit(){
+    this._swaggerService.getLanguages()
+          .subscribe(
+            res => {
+              this._languages = res;
+            },
+            error => { console.log('an error occurred'); }
+          )
+  }
+}
